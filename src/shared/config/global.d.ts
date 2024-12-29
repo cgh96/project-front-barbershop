9b29a5059f77dcf49c9f318ac6fb81efbc9ec2d4
cgh96 <tt878429@gmail.com>
2024-12-29 17:59:24 +0900

    chore : svg icon을 Component처럼 사용할 수 있도록 설정

--- src/shared/config/global.d.ts
+++ src/shared/config/global.d.ts
@@ -3,6 +3,11 @@ declare global {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     __staticRouterHydrationData: any;
   }
+
+  declare module "*.svg" {
+    const content: React.FC<React.SVGProps<SVGElement>>;
+    export default content;
+  }
 }
 
 export {};
