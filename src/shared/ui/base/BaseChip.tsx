5c116fdd5e0544454e4912b7635a5bdcad2f694c
cgh96 <tt878429@gmail.com>
2025-01-05 15:34:48 +0900

    fix : chip design오류 수정

--- src/shared/ui/base/BaseChip.tsx
+++ src/shared/ui/base/BaseChip.tsx
@@ -1,6 +1,6 @@
-import "./BaseChip.scss";
-
 import { BaseChipVariant, type TBaseChipVariant } from "@shared/types";
+
+import styles from "./BaseChip.module.scss";
 
 export interface ChipProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
@@ -17,12 +17,12 @@ export const BaseChip: React.FC<ChipProps> = ({
   variant = BaseChipVariant.Primary,
   ...props
 }: ChipProps) => {
-  const className = `chip ${variant} ${selected ? "selected" : ""}`;
+  const className = `${styles["chip"]} ${styles[variant]} ${selected ? styles["selected"] : ""}`;
 
   return (
     <button className={className} type="button" onClick={props.onClick}>
-      <span className="main-label">{mainLabel}</span>
-      {!!subLabel && <span className="sub-label">{subLabel}</span>}
+      <span className={styles["main-label"]}>{mainLabel}</span>
+      {!!subLabel && <span className={styles["sub-label"]}>{subLabel}</span>}
     </button>
   );
 };
