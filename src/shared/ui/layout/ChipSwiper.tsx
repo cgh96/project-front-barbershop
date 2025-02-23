473789bad3f7aaa176c2749858031531bf665ec2
cgh96 <tt878429@gmail.com>
2025-02-23 14:11:28 +0900

    feat : ChipSwiper추가 - 칩들을 스와이프 할 수 있음

---
+++ src/shared/ui/layout/ChipSwiper.tsx
@@ -0,0 +1,32 @@
+import "swiper/css";
+
+import type { TBaseChipVariant } from "@shared/types";
+import { BaseChip, type ChipProps } from "@shared/ui/chip/BaseChip";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import styles from "./ChipSwiper.module.scss";
+
+export interface ChipSwiperProps {
+  chips: ChipProps[];
+  variant?: TBaseChipVariant;
+}
+
+export const ChipSwiper: React.FC<ChipSwiperProps> = ({ chips, variant }) => {
+  return (
+    <div className={styles["swiper-container"]}>
+      <Swiper spaceBetween={8} slidesPerView={"auto"}>
+        {chips.map((chip, index) => (
+          <SwiperSlide key={index} style={{ width: "auto" }}>
+            <BaseChip
+              key={index}
+              variant={variant ?? chip.variant}
+              mainLabel={chip.mainLabel}
+              subLabel={chip.subLabel}
+              selected={chip.selected}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
